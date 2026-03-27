c0329168f3452c86f624ccd4f935ac350bbf32dc
SmoliconBoi <marcus.dullemond@icloud.com>
2026-03-27 17:00:20 +1000

    cleanup

--- main.lua
+++ main.lua
@@ -19,21 +19,13 @@ AltTexture({
   path = 'splashofcolour.png',
   keys = {'m_bonus', 'm_wild'},
   loc_txt = {
-<<<<<<< HEAD
     name = 'Example Texture',
-=======
-    name = 'Texture',
->>>>>>> 8deba0ad5fe56c692537b6e2979acb9dfe4c4575
   }
 })
 
 TexturePack({
     key = "splash_of_colour",
-<<<<<<< HEAD
     textures = {'soc_enhancements'},
-=======
-     textures = {'soc_enhancements'},
->>>>>>> 8deba0ad5fe56c692537b6e2979acb9dfe4c4575
       loc_txt = {
         name = 'Splash of Colour',
         text = {'Gives {c:attention}Bonus{} and {c:attention}Wild{} cards a new coat of paint,', 'inspired by SoftiBoiled :>'}
